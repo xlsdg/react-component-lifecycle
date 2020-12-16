89832c1e68e8b61ef74e2fac4ba02562a5ab011d
xLsDg <xlsdg@qq.com>
2020-12-16 15:04:15 +0800

    chore(pkg): upgrade deps

--- src/index.js
+++ src/index.js
@@ -4,18 +4,25 @@ import ReactDOM from "react-dom";
 import AppA from "./AppA";
 // import AppB from "./AppB";
 
+ReactDOM.render(
+  <React.StrictMode>
+    <AppA />
+  </React.StrictMode>,
+  document.getElementById("root")
+);
+
 // ReactDOM.render(
 //   <React.StrictMode>
-//     <AppA />
+//     <AppB />
 //   </React.StrictMode>,
 //   document.getElementById("root")
 // );
 
-ReactDOM.createRoot(document.getElementById("root")).render(
-  <React.StrictMode>
-    <AppA />
-  </React.StrictMode>
-);
+// ReactDOM.createRoot(document.getElementById("root")).render(
+//   <React.StrictMode>
+//     <AppA />
+//   </React.StrictMode>
+// );
 
 // ReactDOM.createRoot(document.getElementById("root")).render(
 //   <React.StrictMode>
